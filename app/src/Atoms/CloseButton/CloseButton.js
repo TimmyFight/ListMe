1b91d2cdcbf05da6569756dd4e646698b22bdb7e
timmyfight <gtaaa@wp.pl>
2022-08-05 18:20:34 +0200

    Add logic for close parrent button, add gradient in popup background

--- app/src/Atoms/CloseButton/CloseButton.js
+++ app/src/Atoms/CloseButton/CloseButton.js
@@ -1,7 +1,15 @@
 import styles from "./CloseButton.module.css";
 
 const CloseButton = () => {
-  return <div className={styles.closeButton}>X</div>;
+  const closeParrent = (event) => {
+    event.target.parentNode.classList.add("hidden");
+  };
+
+  return (
+    <div className={styles.closeButton} onClick={closeParrent}>
+      X
+    </div>
+  );
 };
 
 export default CloseButton;
